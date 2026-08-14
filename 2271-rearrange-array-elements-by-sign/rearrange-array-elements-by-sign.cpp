2318b254bda25eb258d3cacfbc14f9fa61bfbe82
Shivam Joshi <shivamjoshi7906@gmail.com>
2026-08-14 23:04:02 +0530

    Time: 17 ms (5.28%) | Memory: 139.1 MB (12.21%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -3,24 +3,24 @@ public:
     vector<int> rearrangeArray(vector<int>& nums) {
         int n = nums.size();
         
-        vector<int> evena;
-        vector<int> odda;
+        vector<int> pos;
+        vector<int> neg;
         for(int i =0;i<n;i++){
             if(nums[i] > 0){
-                evena.push_back(nums[i]);
+                pos.push_back(nums[i]);
             } else {
-                odda.push_back(nums[i]);
+                neg.push_back(nums[i]);
             }
         }
-        int even =0;
-        int odd =0;
+        int ppos =0;
+        int nneg =0;
         for(int i=0;i<n;i++){
             if(i%2==0){
-                nums[i] = evena[even];
-                even++;
+                nums[i] = pos[ppos];
+                ppos++;
             }else{
-                nums[i] = odda[odd];
-                odd++;
+                nums[i] = neg[nneg];
+                nneg++;
             }
             
         }
