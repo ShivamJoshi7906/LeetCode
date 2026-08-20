6b3aee7ebad8c13841ac93544a25e4bb91e2481f
Shivam Joshi <shivamjoshi7906@gmail.com>
2026-08-20 20:26:59 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (34.07%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count = 0;
+        int n = s.size();
+        for(int i=n-1;i>=0;i--){
+            if(s[i] == ' ' && count == 0) {
+                continue;
+            }
+            if(s[i] == ' '){
+                return count;
+                break;
+            }
+            count++;
+        }
+        return count;
+    }
+};
