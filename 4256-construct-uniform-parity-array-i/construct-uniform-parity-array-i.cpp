549cd560207e37685770a06ea6441676466d5faa
Shivam Joshi <shivamjoshi7906@gmail.com>
2026-09-02 11:01:52 +0530

    Time: 1 ms (10.91%) | Memory: 30.3 MB (67.80%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -1,44 +1,7 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        bool hasEven = false;
-        bool hasOdd = false;
-        int odd = 0;
-
-        vector<int> nums2;
-
-        for(int x : nums1) {
-            if(x % 2 == 0) {
-                hasEven = true;
-            } 
-            else {
-                hasOdd = true;
-                odd = x;
-            }
-        }
-
-        // Already uniform
-        if(hasOdd == false || hasEven == false) {
-            return true;
-        }
-
-        // Both odd and even exist
-        for(int x : nums1) {
-            if(x % 2 != 0) {
-                nums2.push_back(x);
-            } 
-            else {
-                nums2.push_back(x - odd);
-            }
-        }
-
-        // Check whether all elements are odd
-        for(int x : nums2) {
-            if(x % 2 == 0) {
-                return false;
-            }
-        }
-
+        
         return true;
     }
 };
